02f0cf77c628931de67ffcb115f379e891cb896c
kalyanbhetwal <kalyanbtl@gmail.com>
2023-06-09 14:34:29 -0600

    adding token class

---
+++ lox/Token.cpp
@@ -0,0 +1,24 @@
+#include "TokenType.cpp"
+#include <string>
+
+class Token{
+    public:
+        TokenType type;
+        std::string lexeme;
+        std::string literal;
+        int line;
+
+        Token(TokenType type, const std::string& lexeme, const std::string& literal, int line){
+            this->type = type;
+            this->lexeme = lexeme;
+            this->literal = literal;
+            this->line = line;
+        }
+        std::string toString(){
+            return std::to_string(static_cast<int>(type)) + " " + lexeme + " "+ literal;
+        }
+
+
+
+
+};
