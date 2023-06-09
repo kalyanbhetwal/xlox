02186ffc70d1bec435ca0637cab01306aa5b6b9b
kalyanbhetwal <kalyanbtl@gmail.com>
2023-06-09 12:46:50 -0600

    read file

--- lox/Lox.cpp
+++ lox/Lox.cpp
@@ -1,11 +1,27 @@
 //#include "Lox.h"
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
 
+void run(const std::string& sourceCode){
+
+}
+// take file as input and run it
 void runFile(const std::string& filePath){
+    std::ifstream file(filePath, std::ios::binary);
+    if(!file){
+        std::cerr << "Failed to open an file: "<<std::endl;
+        return;
+    }
+    std::ostringstream oss;
+    oss << file.rdbuf();
+    std::string sourceCode = oss.str();
+    file.close();
+    run(sourceCode);
     
 }
-
+//run in REPL mode
 void runPrompt(){
 
 }
