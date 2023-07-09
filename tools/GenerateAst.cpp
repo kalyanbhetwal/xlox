48c3d375a485bc4a2cd89336a9f9516c1e8bfdfd
kalyanbhetwal <kalyanbtl@gmail.com>
2023-07-09 12:21:20 -0600

    adding define ast

--- tools/GenerateAst.cpp
+++ tools/GenerateAst.cpp
@@ -4,6 +4,24 @@
 #include <string>
 using namespace std;
 
+void defineAst(const string& outputDir, const string& baseName, const vector<string>& types) {
+    string path = outputDir + "/" + baseName + ".cpp";
+    ofstream output(path);
+    if (!output) {
+        cerr << "Unable to create file: " << path << endl;
+        return;
+    }
+
+    output << "#include \"" << baseName << ".hpp\"" << endl << endl;
+    output << "using namespace std;" << endl << endl;
+    output << "class " << baseName << " {" << endl;
+    output << "public:" << endl;
+
+    output << "};" << endl << endl;
+
+    output.close();
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         cerr << "Usage: generate_ast <output directory>" << endl;
@@ -11,11 +29,13 @@ int main(int argc, char* argv[]) {
     }
 
     string outputDir = argv[1];
-    vector<string> expressions = {
+    vector<string> types = {
         "Binary   : Expr* left, Token* op, Expr* right",
         "Grouping : Expr* expression",
         "Literal  : Object* value",
         "Unary    : Token* op, Expr* right"
     };
+
+    defineAst(outputDir, "Expr", types);
     return 0;
 }
